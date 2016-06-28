73ad68e62ebfdc9be7b3610707b41e1a819863ac
Alexandre Sadowski <asadowski@beapi.fr>
2016-06-28 15:52:47 +0200

    add filter on wp_get_attachment_image_attributes to get mode and render

--- classes/main.php
+++ classes/main.php
@@ -1,6 +1,9 @@
 <?php
 namespace ARI;
 
+use ARI\Image_Locations;
+use ARI\Modes;
+
 /**
  * The purpose of the main class is to init all the plugin base code like :
  *  - Taxonomies
@@ -20,6 +23,7 @@ class Main {
 
 	protected function init() {
 		add_action( 'init', array( $this, 'init_translations' ) );
+		add_filter( 'wp_get_attachment_image_attributes', array( $this, 'get_attributes' ), 10, 2 );
 	}
 
 	/**
@@ -28,6 +32,44 @@ class Main {
 	public static function init_translations() {
 		// Load translations
 		load_plugin_textdomain( 'ari', false, ARI_DIR . 'languages' );
+	}
+
+	/**
+	 * @param array $args
+	 * @param \WP_Post $attachment
+	 *
+	 * @return array
+	 * @author Alexandre Sadowski
+	 */
+	public function get_attributes( $args = array(), \WP_Post $attachment ) {
+		if ( ! isset( $args['data-location'] ) ) {
+			$args['data-location'] = 'No location filled in';
+			return $args;
+		}
+
+		/**
+		 * @var $locations Image_Locations
+		 */
+		$locations      = Image_Locations::get_instance();
+		$location_array = $locations->get_location( $args['data-location'] );
+		if ( empty( $location_array ) ) {
+			$args['data-location'] = 'No location found in source file';
+			return $args;
+		}
+
+		/**
+		 * @var $mode Modes
+		 */
+		$mode = Modes::get_instance();
+		try {
+			$_mode_instance = $mode->get_mode( $args );
+			$_mode_instance->set_attachment_id( $attachment->ID );
+			$_mode_instance->add_filters();
+		} catch ( \Exception $e ) {
+			$args['data-location'] = $e->getMessage();
+		}
+
+		return $args;
 	}
 
 }
